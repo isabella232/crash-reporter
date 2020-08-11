058955b5e4dd12250b9ae463c5cb662d1378dc0c
Paul Rouget <me@paulrouget.com>
2020-08-11 11:32:07 +0200

    Add tokens for writing and reading reports

--- crash_reporter/flask_server.py
+++ crash_reporter/flask_server.py
@@ -1,15 +1,25 @@
 from datetime import datetime
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, abort
+from flask_httpauth import HTTPBasicAuth
 from os import listdir
 from os.path import exists, isfile, join
 import json
-app = Flask(__name__)
 
-crash_dir = None
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    if password == config['read_secret']:
+        return "valid_user"
 
 @app.route("/report", methods=["POST"])
 def report():
     report = request.get_json()
+    if 'secret' not in report or report['secret'] != config['write_secret']:
+        abort(403)
+        return
+    del report['secret']
     id = 0
     idstr = None
     now = datetime.now()
@@ -18,7 +28,7 @@ def report():
     while True:
       idstr = str(id).zfill(4)
       filename = 'crash-' + date + "-" +  idstr + '.json'
-      crashfile = join(crash_dir, filename)
+      crashfile = join(config['crash_dir'], filename)
       try:
           fd = open(crashfile, 'x')
           json.dump(report, fd, indent=2)
@@ -30,6 +40,7 @@ def report():
 
 @app.route("/list", methods=["GET"])
 def list():
+    crash_dir = config['crash_dir']
     crash_files = [f for f in listdir(crash_dir) if isfile(join(crash_dir, f)) and f.startswith("crash-")]
     crash_ids = map(lambda f: f.replace("crash-","").replace(".json", ""), crash_files)
     lis = map(lambda i: '<li><a href="./query?id=' + i + '">' + i + '</a></li>', crash_ids)
@@ -37,21 +48,25 @@ def list():
     return (html, 200)
 
 @app.route("/query", methods=["GET"])
+@auth.login_required
 def query():
     if 'id' in request.args:
         id = request.args['id']
         filename = 'crash-' + id + '.json'
-        crashfile = join(crash_dir, filename)
-        return send_file(crashfile)
-    return ('', 404)
+        crashfile = join(config['crash_dir'], filename)
+        if exists(crashfile):
+          return send_file(crashfile)
+        return ('Report not found', 404)
+    return ('Missing ID', 400)
 
 def main():
+    global config
     with open('config.json') as f:
         config = json.loads(f.read())
         assert('port' in config)
         assert('crash_dir' in config)
-        global crash_dir
-        crash_dir = config['crash_dir']
+        assert('read_secret' in config)
+        assert('write_secret' in config)
     app.run(port=config['port'])
 
 if __name__ == "__main__":
